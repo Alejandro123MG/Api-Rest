5680b0567206695123b80b617529c3d5e6d2e514
Alejandro Monsivais 20100239 <alejandromonsivais367@gmail.com>
2025-02-06 10:49:18 -0600

    cambios

--- Parcial1/ServerExpress/server.js
+++ Parcial1/ServerExpress/server.js
@@ -2,11 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use('/',(req,res,next) => {
+    console.log("peticon recibida");
+    next();
+}, (req, res, next) => {
+    console.log("2da funcion middleware");
+    next();
+});
+
+//middleware
+app.use(express.json());
+app.use(express.text());
+
 app.get('/', (req, res, next) => {
     res.sendFile(__dirname+'/public/index.html');
-    
-    
 });
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('hello world');
+});
+
+app.use((req,res)=>{
+    res.status(404);
+    res.send("error 404");
+})
 
 app.listen(port, () => {
     console.log(`corriendo servidor http://localhost:${port}`);
--- Parcial1/servidorhttp/index.js
+++ Parcial1/servidorhttp/index.js
@@ -1,9 +1,25 @@
-var http = require('http');
+const http = require("http");
 
-let server = http.createServer((req, res) => {
-    res.writeHead(200, {'Access-Control-Allow-Origin': '*'});
-    res.end('Hello World\n');
+const PORT = 3001;
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/sin-cors") {
+        // No incluye encabezado CORS (esto causará un error)
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Respuesta sin CORS");
+    } else if (req.url === "/con-cors") {
+        // Incluye encabezado CORS (esto funcionará)
+        res.writeHead(200, {
+            "Content-Type": "text/plain",
+            "Access-Control-Allow-Origin": "*"
+        });
+        res.end("Respuesta con CORS");
+    } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Ruta no encontrada");
+    }
 });
-server.listen(3000, ()=>{
-    console.log('Server running at http://localhost:3000/');
+
+server.listen(PORT, () => {
+    console.log(`Servidor 2 escuchando en http://localhost:${PORT}`);
 });
