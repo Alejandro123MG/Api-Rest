2d649c566a55c1712811433974454ef61b48c60a
Alejandro Monsivais 20100239 <alejandromonsivais367@gmail.com>
2025-02-07 09:31:26 -0600

    tarea Recepcion de Datos y Middleware

--- Parcial1/ServerExpress/server.js
+++ Parcial1/ServerExpress/server.js
@@ -3,10 +3,10 @@ const app = express();
 const port = 3000;
 
 app.use('/',(req,res,next) => {
-    console.log("peticon recibida");
+    console.log("--peticion recibida");
     next();
 }, (req, res, next) => {
-    console.log("2da funcion middleware");
+    console.log("2da funcion middleware--");
     next();
 });
 
@@ -14,11 +14,16 @@ app.use('/',(req,res,next) => {
 app.use(express.json());
 app.use(express.text());
 
-app.get('/', (req, res, next) => {
+app.get('/alumnos', (req, res) => {
     res.sendFile(__dirname+'/public/index.html');
 });
 
-app.post('/', (req, res) => {
+app.post('/sistemas/:control', (req, res) => {
+    console.log(req.body);
+    res.send('hello world');
+});
+
+app.patch('/materia', (req, res) => {
     console.log(req.body);
     res.send('hello world');
 });
