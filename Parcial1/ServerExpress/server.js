6297229294f1c13e1020dd263f4c0cb482a06508
Alejandro Monsivais 20100239 <alejandromonsivais367@gmail.com>
2025-02-14 09:42:42 -0600

    xml y formularios

--- Parcial1/ServerExpress/server.js
+++ Parcial1/ServerExpress/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+xmlparser = require('express-xml-bodyparser');
+const multer  = require('multer'); 
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use('/',(req,res,next) => {
     console.log("--peticion recibida");
@@ -13,6 +16,11 @@ app.use('/',(req,res,next) => {
 //middleware
 app.use(express.json());
 app.use(express.text());
+app.use(xmlparser());
+
+const folder = path.join(__dirname+'/archivosrec/'); //Definir una ruta para almacenar archivos que se envian del cliente
+ const upload = multer({dest:folder});             
+ app.use(upload.single('archivo')); 
 
 app.get('/alumnos', (req, res) => {
     res.sendFile(__dirname+'/public/index.html');
@@ -26,6 +34,17 @@ app.post('/sistemas/:control', (req, res) => {
 app.patch('/materia', (req, res) => {
     console.log(req.body);
     res.send('hello world');
+});
+
+app.post('/prefectos', (req, res) => {
+        // console.log(req.body);
+        // res.send('hello world');
+
+        console.log(`Se recibio el archivo: ${req.file.originalname}`); 
+        console.log(req.body); 
+        console.log('Se recibio el formulario:'+JSON.stringify(req.body)); 
+        res.json(req.body);
+  
 });
 
 app.use((req,res)=>{
