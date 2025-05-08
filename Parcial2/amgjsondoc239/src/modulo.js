fc1772a46d004dcabf112c8e115ded4bf8cdcd9b
Alejandro Monsivais 20100239 <alejandromonsivais367@gmail.com>
2025-05-08 09:22:25 -0500

    parcial3.2

---
+++ Parcial2/amgjsondoc239/src/modulo.js
@@ -0,0 +1,14 @@
+
+/**
+*@param {Number} a - Primer número
+*@param {Number} b - Segundo número
+*@returns {Number} 
+ */
+
+function sumar(a, b) {
+    return a + b;
+}
+
+module.exports = {
+    sumar: sumar
+};
